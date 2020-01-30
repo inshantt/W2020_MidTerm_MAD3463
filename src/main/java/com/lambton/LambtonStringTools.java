bb77e247caf82c448a1a348b67dca7f93e546edf
Pruthviraj Chauhan <chauhanpruthviraj02@gmail.com>
2020-01-29 19:27:40 -0500

    [inshant tageja] answer 3 aa

--- src/main/java/com/lambton/LambtonStringTools.java
+++ src/main/java/com/lambton/LambtonStringTools.java
@@ -9,7 +9,7 @@ package com.lambton;
  *
  * @author Pritesh Patel
  */
-public class LambtonStringTools 
+public class LambtonStringTools
 {
     //1 - REVERSE STRING
     public static String reverse(String s)
@@ -24,12 +24,12 @@ public class LambtonStringTools
         return rev;
 
     }
-    
+
     //2 - FORMAT INITIALS OF STRING
-    public static String initials(String s) 
+    public static String initials(String s)
     {
         String[] words = s.split("\\s+");
-        int i = 0;
+
         String cap = "";
         if (words.length == 3) {
             cap = words[0].substring(0, 1).toUpperCase() + ". "
@@ -40,19 +40,35 @@ public class LambtonStringTools
         }
         return cap;
     }
-        
+
     //3 - FIND MOST FREQUENT CHARACTER FROM STRING
-    public static String mostFrequent(String s) 
+    public static char mostFrequent(String s)
     {
-        return null;
+
+        int ccount = 256;
+        int[] count = new int[ccount];
+        int i;  //used in loops
+        for (i = 0; i < s.length(); i++)
+            (count[s.charAt(i)])++;
+        int mostFrequently = 0;
+        for (i = 0; i < ccount; i++) {
+            if (count[i] > count[mostFrequently]) {
+                mostFrequently = i;
+            } else if (count[i] == count[mostFrequently]) {
+                mostFrequently = i;
+            }
+        }
+        return (char) mostFrequently;
     }
-    
+
+    }
+
     //4 - CONVERT BINARY NUMBER TO DECIMALS
-    public static int binaryToDecimal(String s) 
+    public static int binaryToDecimal(String s)
     {
        return 0;
     }
-     
+
     //5 - REPLACING SUBSTRING WITH NEW STRING IN EXISTING STRING
     public static String replaceSubString(String originalString, String findString, String newString)
     {
