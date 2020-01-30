68c3d6384e18bbfce8caa93a90447128c72a0cf6
Pruthviraj Chauhan <chauhanpruthviraj02@gmail.com>
2020-01-29 19:37:06 -0500

    [inshant tageja] answer 3 change with output

--- src/main/java/com/lambton/LambtonStringTools.java
+++ src/main/java/com/lambton/LambtonStringTools.java
@@ -13,32 +13,37 @@ public class LambtonStringTools
 {
     //1 - REVERSE STRING
     public static String reverse(String s)
-    {
-        char ch[] = s.toCharArray();
-        String rev = " ";
+    {  String rev = "";
+        if(s!=null) {
+            char ch[] = s.toCharArray();
 
-        for (int i = ch.length - 1; i >= 0; i--)
-        {
-            rev += ch[i];
+            for (int i = ch.length - 1; i >= 0; i--) {
+                rev += ch[i];
+            }
+
+
+        }
+        else{
+            rev="Output"+rev;
         }
         return rev;
-
     }
 
     //2 - FORMAT INITIALS OF STRING
     public static String initials(String s)
     {
-        String[] words = s.split("\\s+");
+        String[] word = s.split("\\s+");
 
-        String cap = "";
-        if (words.length == 3) {
-            cap = words[0].substring(0, 1).toUpperCase() + ". "
-                    + words[1].substring(0, 1).toUpperCase() + ". "
-                    + words[2].substring(0, 1).toUpperCase() + words[2].substring(1).toLowerCase();
+        String capital = "";
+        if (word.length == 3) {
+            capital = word[0].substring(0, 1).toUpperCase() + ". "
+                    + word[1].substring(0, 1).toUpperCase() + ". "
+                    + word[2].substring(0, 1).toUpperCase() + word[2].substring(1).toLowerCase();
         } else {
-            cap = null;
+            capital = null;
         }
-        return cap;
+        return capital;
+
     }
 
     //3 - FIND MOST FREQUENT CHARACTER FROM STRING
@@ -47,7 +52,7 @@ public class LambtonStringTools
 
         int ccount = 256;
         int[] count = new int[ccount];
-        int i;  //usedinloops
+        int i;
         for (i = 0; i < s.length(); i++)
             (count[s.charAt(i)])++;
         int mostFrequently = 0;
@@ -61,22 +66,27 @@ public class LambtonStringTools
         return (char) mostFrequently;
     }
 
-    }
-
     //4 - CONVERT BINARY NUMBER TO DECIMALS
     public static int binaryToDecimal(String s)
     {
         int decimal=0;
 
-        decimal = Integer.parseInt(s, 2); //binarytodec
+        decimal = Integer.parseInt(s, 2);
 
         return decimal;
-
 
     }
 
     //5 - REPLACING SUBSTRING WITH NEW STRING IN EXISTING STRING
     public static String replaceSubString(String originalString, String findString, String newString)
     {
-
+        originalString= originalString.toLowerCase();
+        findString = findString.toLowerCase();
+        newString = newString.toLowerCase();
+        if (originalString.contains(findString)) {
+            originalString = originalString.replace(findString, newString);
+            return originalString;
+        } else
+            return "String doesn't contain matched pattern";
+    }
 }
